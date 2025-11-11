adc3c6031ebad043b2a198eb031ba8df198d3e74
femcloud2025 <femcloud2025@gmail.com>
2025-11-11 17:20:33 +0530

    Update productApi.js

--- user/src/api/productApi.js
+++ user/src/api/productApi.js
@@ -1,4 +1,4 @@
 export async function getAllProducts() {
-  const res = await fetch("http://localhost:4000/api/products");
+  const res = await fetch("https://femcloudfinal2025.onrender.com/api/products");
   return res.json(); // [{ _id, name, description, price, image, category, whatsappNumber, seller }]
 }
